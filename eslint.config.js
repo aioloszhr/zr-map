5105b8d8e20ab5a0a709497fbd6259fe6dd791f4
aioloszhr <aioloszhr@aioloszhrdeMacBook-Pro.local>
2024-09-29 19:58:45 +0800

    feat: 项目初始化

---
+++ eslint.config.js
@@ -0,0 +1,48 @@
+import eslint from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import eslintPluginVue from 'eslint-plugin-vue'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
+
+export default tseslint.config(
+  { ignores: ['node_modules', 'dist', 'public'] },
+  eslint.configs.recommended,
+  /** ts推荐配置 */
+  ...tseslint.configs.recommended,
+  ...eslintPluginVue.configs['flat/recommended'],
+  {
+    languageOptions: {
+      globals: { ...globals.browser },
+    },
+  },
+  /** javascript规则 */
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    rules: {
+      'no-console': 'error',
+    },
+  },
+  /** vue规则 */
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        /** typescript项目需要用到这个 */
+        parser: tseslint.parser,
+        ecmaVersion: 'latest',
+        /** 允许在.vue 文件中使用 JSX */
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {},
+  },
+  /** typescript规则 */
+  {
+    files: ['**/*.{ts,tsx,vue}'],
+    rules: {},
+  },
+  /** prettier配置 */
+  eslintPluginPrettier,
+)
